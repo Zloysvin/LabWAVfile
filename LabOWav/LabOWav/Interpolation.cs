e4c00edf51eda26c8080e130e9f62a56e8221199
Zloysvin <danylomartakov@gmail.com>
2021-04-28 02:25:21 +0300

    base methods commit

---
+++ LabOWav/LabOWav/Interpolation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabOWav
+{
+    class Interpolation
+    {
+        public void Execute()
+        {
+
+        }
+
+        private int[] readData(int byteStream, byte[] data)
+        {
+
+
+            return null;
+        }
+    }
+}
